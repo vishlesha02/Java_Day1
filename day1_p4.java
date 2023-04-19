e5f43ee9c86f2242842575a77693352dbc3073be
vishlesha02 <69750154+vishlesha02@users.noreply.github.com>
2023-04-19 12:36:21 +0530

    Add files via upload

---
+++ day1_p4.java
@@ -0,0 +1,15 @@
+public class day1_p4{
+public static void main(String args[])
+{
+	int a=Integer.parseInt(args[0]);
+	int b=Integer.parseInt(args[1]);
+	int sum;
+
+	sum=a+b;
+
+System.out.println("Sum of a+b = " + sum);
+
+	
+}
+
+}
---
+++ day1_p5.java
@@ -0,0 +1,12 @@
+public class day1_p5
+{
+public static void main(String [] args)
+{
+        String name="";
+        name=args[0];
+
+        System.out.println("Name : " +name);
+
+
+}
+}
